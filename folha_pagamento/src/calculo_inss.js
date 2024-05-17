4bf85a2d9ce3db5bda4c45fd3e3aafdbd39199ff
LeoSntos <leobrognoli38@gmail.com>
2024-05-17 18:06:48 -0300

    exercicio 2

---
+++ folha_pagamento/src/calculo_inss.js
@@ -0,0 +1,22 @@
+function calcularINSS(salarioBruto) {
+    const tetoINSS = 908.85;
+    let valorINSS = 0;
+
+    if (salarioBruto <= 1412.00) {
+        valorINSS = salarioBruto * 0.075;
+    } else if (salarioBruto <= 2666.68) {
+        valorINSS = (1412.00 * 0.075) + ((salarioBruto - 1412.00) * 0.09);
+    } else if (salarioBruto <= 4000.03) {
+        valorINSS = (1412.00 * 0.075) + ((2666.68 - 1412.00) * 0.09) + ((salarioBruto - 2666.68) * 0.12);
+    } else if (salarioBruto <= 7786.02) {
+        valorINSS = (1412.00 * 0.075) + ((2666.68 - 1412.00) * 0.09) + ((4000.03 - 2666.68) * 0.12) + ((salarioBruto - 4000.03) * 0.14);
+    } else {
+        valorINSS = tetoINSS;
+    }
+
+    return valorINSS;
+}
+
+const salarioBruto = 6000;
+const valorINSS = calcularINSS(salarioBruto);
+console.log(`O valor do INSS a ser pago é: R$ ${valorINSS.toFixed(2)}`);
